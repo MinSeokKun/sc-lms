08ab09573701f99e4eb183431da526e2718cef41
MinSeokKun <kimms51567115@gmail.com>
2024-06-26 09:10:51 +0900

    Note
    노트 레포, 서비스, 컨트롤러, 템플릿 작성
    노트 리스트

--- sc/src/main/java/com/lms/sc/controller/NoteController.java
+++ sc/src/main/java/com/lms/sc/controller/NoteController.java
@@ -1,23 +1,55 @@
 package com.lms.sc.controller;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.lms.sc.entity.Lecture;
+import com.lms.sc.entity.Note;
+import com.lms.sc.entity.Video;
+import com.lms.sc.service.NoteService;
+
 import lombok.RequiredArgsConstructor;
+
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/note")
 public class NoteController {
 	
+	private final NoteService noteService;
+	
 	@GetMapping("/list/{userId}")
 	public String getNoteList(@PathVariable("userId") long userId, Model model) {
+		List<Lecture> noteLecture = noteService.getNoteLecture(userId);
 		
+		Map<Lecture, Integer> noteList = new HashMap<Lecture, Integer>();
+		noteLecture.forEach(lecture -> 
+			noteList.put(lecture, noteService.getByLecture(lecture.getId(), userId).size()));
+		model.addAttribute("userId", userId);
+		model.addAttribute("noteList", noteList);
 		return "note/note_list";
 	}
+	
+	@GetMapping("/list/{userId}/{lecId}")
+	public String getMethodName(@PathVariable("userId") long userId,
+			@PathVariable("lecId") long lecId, Model model) {
+		List<Video> videoList = noteService.getVideosByLecture(lecId, userId);
+		Map<Video, List<Note>> noteList = new HashMap<Video, List<Note>>();
+		
+		videoList.forEach(video -> noteList.put(video, noteService.getByVideo(video.getId(), userId)));
+		
+		model.addAttribute("noteList", noteList);
+		
+		return "note/note_lecture";
+	}
+	
 	
 	
 }
--- sc/src/main/java/com/lms/sc/service/NoteService.java
+++ sc/src/main/java/com/lms/sc/service/NoteService.java
@@ -1,13 +1,19 @@
 package com.lms.sc.service;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
+import com.lms.sc.entity.Lecture;
 import com.lms.sc.entity.Note;
 import com.lms.sc.entity.SiteUser;
 import com.lms.sc.entity.Video;
+import com.lms.sc.repository.LectureRepository;
 import com.lms.sc.repository.NoteRepository;
+import com.lms.sc.repository.UserRepository;
+import com.lms.sc.repository.VideoRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -15,7 +21,9 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class NoteService {
 	private final NoteRepository noteRepository;
-	
+	private final UserRepository userRepository;
+	private final LectureRepository lectureRepository;
+	private final VideoRepository videoRepository;
 	// 노트생성
 	public Note createNote(String content, long videoTime, SiteUser author, Video video) {
 		Note note = new Note();
@@ -33,5 +41,38 @@ public class NoteService {
 		noteRepository.delete(note);
 	}
 	
+	// 노트 리스트
+	public List<Note> getNoteList(long userId){
+		Optional<SiteUser> user = userRepository.findById(userId);
+		List<Note> noteList = noteRepository.findByAuthor(user.get());
+		return noteList;
+	}
+	
+	// 노트 강의 리스트
+	public List<Lecture> getNoteLecture(long userId) {
+		SiteUser user = userRepository.findById(userId).get();
+		return noteRepository.findLecturesByAuthor(user);
+	}
+	
+	// 노트 영상 리스트
+	public List<Video> getVideosByLecture(long lecId, long userId){
+		SiteUser author = userRepository.findById(userId).get();
+		Lecture lecture = lectureRepository.findById(lecId).get();
+		return noteRepository.findVideosByAuthorAndLecture(lecture, author);
+	}
+	
+	// 영상으로 찾는 노트 리스트
+	public List<Note> getByVideo(long videoId, long userId){
+		SiteUser author = userRepository.findById(userId).get();
+		Video video = videoRepository.findById(videoId).get();
+		return noteRepository.findByVideoAndAuthor(video, author);
+	}
+	
+	// 강의로 찾는 노트 리스트
+	public List<Note> getByLecture(long lecId, long userId) {
+		SiteUser author = userRepository.findById(userId).get();
+		Lecture lecture = lectureRepository.findById(lecId).get();
+		return noteRepository.findByLectureAndAuthor(lecture, author);
+	}
 	
 }

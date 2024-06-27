c1d58a3feb75599d55b36da6130524b0f710e200
yeop9701 <toa444@nate.com>
2024-06-27 17:28:54 +0900

    페이징
    24.06.27

--- sc/src/main/java/com/lms/sc/service/QuestionService.java
+++ sc/src/main/java/com/lms/sc/service/QuestionService.java
@@ -4,6 +4,9 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.lms.sc.entity.Question;
@@ -16,6 +19,11 @@ import lombok.RequiredArgsConstructor;
 @Service
 public class QuestionService {
 	private final QuestionRepository questionReqRepository;
+	
+	public Page<Question> getListPaging(int page){
+		Pageable pageable = PageRequest.of(page, 10);
+		return this.questionReqRepository.findAll(pageable);
+	}
 	
 	public List<Question> getList() {
 		return this.questionReqRepository.findAll();

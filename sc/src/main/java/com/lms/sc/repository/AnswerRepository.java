26ec1c025d5896555614250e4e0af6fcf049127b
MinSeokKun <kimms51567115@gmail.com>
2024-07-06 18:12:09 +0900

    강의 영상 진행도
    진행도랑 대시보드 약간 수정

--- sc/src/main/java/com/lms/sc/repository/AnswerRepository.java
+++ sc/src/main/java/com/lms/sc/repository/AnswerRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.lms.sc.entity.Answer;
 import com.lms.sc.entity.Question;
 
+
 public interface AnswerRepository extends JpaRepository<Answer, Integer>{
 	List<Answer> findAllByQuestion(Question question);
 }
